b22c5384bb955741639321e4309c24589b633cfb
MoEhab74 <mohamedehabswe74@gmail.com>
2026-03-05 23:07:14 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
