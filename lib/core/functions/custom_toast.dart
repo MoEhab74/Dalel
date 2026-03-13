018dd66d0426b0b83d2f8559030354d3f380785b
MoEhab74 <mohamedehabswe74@gmail.com>
2026-03-14 00:23:15 +0200

    Finish signup and handle states

---
+++ lib/core/functions/custom_toast.dart
@@ -0,0 +1,15 @@
+import 'package:dalel/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showFlutterToast(String msg, Color? backgroundColor) {
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: backgroundColor ?? AppColors.primaryColor,
+    textColor: Colors.white,
+    fontSize: 16.0,
+  );
+}
--- lib/features/auth/presentation/widgets/custom_sign_up_form.dart
+++ lib/features/auth/presentation/widgets/custom_sign_up_form.dart
@@ -1,3 +1,5 @@
+import 'package:dalel/core/functions/custom_toast.dart';
+import 'package:dalel/core/routes/app_routes.dart';
 import 'package:dalel/core/utils/app_colors.dart';
 import 'package:dalel/core/utils/app_strings.dart';
 import 'package:dalel/core/widgets/app_buttom.dart';
@@ -7,6 +9,7 @@ import 'package:dalel/features/auth/presentation/widgets/custom_text_form_field.
 import 'package:dalel/features/auth/presentation/widgets/terms_and_conditions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignUpForm extends StatelessWidget {
   const CustomSignUpForm({super.key});
@@ -15,7 +18,13 @@ class CustomSignUpForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) {
-        // TODO: implement listener
+        if (state is SignUpSuccessState) {
+          showFlutterToast('User signed up successfully', Colors.green);
+          // Pushreplacement to login view
+          // GoRouter.of(context).pushReplacement(AppRoutes.signInRoute);
+        } else if (state is SignUpFailureState) {
+          showFlutterToast(state.errorMessage, Colors.red);
+        }
       },
       builder: (context, state) {
         AuthCubit authCubit = BlocProvider.of<AuthCubit>(context);
@@ -54,20 +63,22 @@ class CustomSignUpForm extends StatelessWidget {
               ),
               TermsAndConditions(),
               SizedBox(height: 170),
-              AppButton(
-                backgroundColor:
-                    authCubit.termsAndConditionsCheckBoxValue == false
-                    ? AppColors.gray
-                    : null,
-                text: AppStrings.signUp,
-                onPressed: () {
-                  if (authCubit.termsAndConditionsCheckBoxValue == true) {
-                    if (authCubit.formKey.currentState!.validate()) {
-                      authCubit.signUpWithEmailAndPassword();
-                    }
-                  }
-                },
-              ),
+              state is SignUpLoadingState
+                  ? CircularProgressIndicator(color: AppColors.primaryColor)
+                  : AppButton(
+                      backgroundColor:
+                          authCubit.termsAndConditionsCheckBoxValue == false
+                          ? AppColors.gray
+                          : null,
+                      text: AppStrings.signUp,
+                      onPressed: () {
+                        if (authCubit.termsAndConditionsCheckBoxValue == true) {
+                          if (authCubit.formKey.currentState!.validate()) {
+                            authCubit.signUpWithEmailAndPassword();
+                          }
+                        }
+                      },
+                    ),
             ],
           ),
         );
